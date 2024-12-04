faadd277475f72d758086580e1ecb19e41b7520b
jacksonmwadia <mwadiajackson@gmail.com>
2024-12-04 20:28:52 +0300

    push my final project from local repository to github

---
+++ README.md
@@ -0,0 +1,40 @@
+# JACK-PAA Inventory
+
+JACK-PAA Inventory is an inventory management system designed to help users track inventory items such as stationery, food, and more. Built with Python and the Django framework, the system supports multiple user roles, including super admin, staff, and customers, with distinct functionalities for each.
+
+---
+
+## Features
+
+1. **User Management**  
+   - Role-based access for super admin, staff, and customers.
+   - Secure login and authentication system.
+
+2. **Inventory Tracking**  
+   - Add, update, delete, and view inventory items in real-time.
+
+3. **Product Categorization**  
+   - Organize items into categories for easier navigation.
+
+4. **Stock Alerts**  
+   - Notifications for low-stock items to ensure timely restocking.
+
+5. **Reporting**  
+   - Generate reports for inventory levels and trends.
+
+6. **User-Friendly Interface**  
+   - Intuitive design for seamless interaction across all user roles.
+
+7. **Secure Data Management**  
+   - Data encryption and secure storage.
+
+8. **Scalability**  
+   - Designed to integrate with future systems or services.
+
+---
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+
---
+++ dashboard/views.py
@@ -0,0 +1,208 @@
+import json
+
+import requests
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from requests.auth import HTTPBasicAuth
+
+from .credentials import MpesaAccessToken, LipanaMpesaPpassword
+from .models import Product, Order
+from .forms import ProductForm, OrderForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .decorators import auth_users, allowed_users
+# Create your views here.
+
+
+# @login_required(login_url='user-login')
+def index(request):
+    product = Product.objects.all()
+    product_count = product.count()
+    order = Order.objects.all()
+    order_count = order.count()
+    customer = User.objects.filter(groups=2)
+    customer_count = customer.count()
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.customer = request.user
+            obj.save()
+            return redirect('dashboard-index')
+    else:
+        form = OrderForm()
+    context = {
+        'form': form,
+        'order': order,
+        'product': product,
+        'product_count': product_count,
+        'order_count': order_count,
+        'customer_count': customer_count,
+    }
+    return render(request, 'dashboard/index.html', context)
+
+
+# @login_required(login_url='user-login')
+def products(request):
+    product = Product.objects.all()
+    product_count = product.count()
+    customer = User.objects.filter(groups=2)
+    customer_count = customer.count()
+    order = Order.objects.all()
+    order_count = order.count()
+    product_quantity = Product.objects.filter(name='')
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            product_name = form.cleaned_data.get('name')
+            messages.success(request, f'{product_name} has been added')
+            return redirect('dashboard-products')
+    else:
+        form = ProductForm()
+    context = {
+        'product': product,
+        'form': form,
+        'customer_count': customer_count,
+        'product_count': product_count,
+        'order_count': order_count,
+    }
+    return render(request, 'dashboard/products.html', context)
+
+
+# @login_required(login_url='user-login')
+def product_detail(request, pk):
+    context = {
+
+    }
+    return render(request, 'dashboard/products_detail.html', context)
+
+
+# @login_required(login_url='user-login')
+# @allowed_users(allowed_roles=['Admin'])
+def customers(request):
+    customer = User.objects.filter(groups=2)
+    customer_count = customer.count()
+    product = Product.objects.all()
+    product_count = product.count()
+    order = Order.objects.all()
+    order_count = order.count()
+    context = {
+        'customers': customer,
+        'customer_count': customer_count,
+        'product_count': product_count,
+        'order_count': order_count,
+    }
+    return render(request, 'dashboard/customers.html', context)
+
+
+# @login_required(login_url='user-login')
+# @allowed_users(allowed_roles=['Admin'])
+def customer_detail(request, pk):
+    customer = User.objects.filter(groups=2)
+    customer_count = customer.count()
+    product = Product.objects.all()
+    product_count = product.count()
+    order = Order.objects.all()
+    order_count = order.count()
+    customers = User.objects.get(id=pk)
+    context = {
+        'customers': customers,
+        'customer_count': customer_count,
+        'product_count': product_count,
+        'order_count': order_count,
+
+    }
+    return render(request, 'dashboard/customers_detail.html', context)
+
+
+# @login_required(login_url='user-login')
+# @allowed_users(allowed_roles=['Admin'])
+def product_edit(request, pk):
+    item = Product.objects.get(id=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard-products')
+    else:
+        form = ProductForm(instance=item)
+    context = {
+        'form': form,
+    }
+    return render(request, 'dashboard/products_edit.html', context)
+
+
+# @login_required(login_url='user-login')
+# @allowed_users(allowed_roles=['Admin'])
+def product_delete(request, pk):
+    item = Product.objects.get(id=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('dashboard-products')
+    context = {
+        'item': item
+    }
+    return render(request, 'dashboard/products_delete.html', context)
+
+
+# @login_required(login_url='user-login')
+def order(request):
+    order = Order.objects.all()
+    order_count = order.count()
+    customer = User.objects.filter(groups=2)
+    customer_count = customer.count()
+    product = Product.objects.all()
+    product_count = product.count()
+
+    context = {
+        'order': order,
+        'customer_count': customer_count,
+        'product_count': product_count,
+        'order_count': order_count,
+    }
+    return render(request, 'dashboard/order.html', context)
+
+
+def token(request):
+    consumer_key = '77bgGpmlOxlgJu6oEXhEgUgnu0j2WYxA'
+    consumer_secret = 'viM8ejHgtEmtPTHd'
+    api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+
+    r = requests.get(api_URL, auth=HTTPBasicAuth(
+        consumer_key, consumer_secret))
+    mpesa_access_token = json.loads(r.text)
+    validated_mpesa_access_token = mpesa_access_token["access_token"]
+
+    return render(request, 'token.html', {"token":validated_mpesa_access_token})
+
+def pay(request):
+   return render(request, 'pay.html')
+
+
+def stk(request):
+    if request.method =="POST":
+        phone = request.POST['phone']
+        amount = request.POST['amount']
+        access_token = MpesaAccessToken.validated_mpesa_access_token
+        api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+        headers = {"Authorization": "Bearer %s" % access_token}
+        request = {
+            "BusinessShortCode": LipanaMpesaPpassword.Business_short_code,
+            "Password"
+            "": LipanaMpesaPpassword.decode_password,
+            "Timestamp": LipanaMpesaPpassword.lipa_time,
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": amount,
+            "PartyA": phone,
+            "PartyB": LipanaMpesaPpassword.Business_short_code,
+            "PhoneNumber": phone,
+            "CallBackURL": "https://sandbox.safaricom.co.ke/mpesa/",
+            "AccountReference": "eMobilis",
+            "TransactionDesc": "Web Development Charges"
+        }
+        response = requests.post(api_url, json=request, headers=headers)
+        return HttpResponse("Success")
+
